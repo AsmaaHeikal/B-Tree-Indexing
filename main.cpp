d77c6148cbc94aac4dab7acd0c5c920213bf6bdc
Asmaa Heikal <AsmaaIHeikal@gmail.com>
2023-12-19 21:42:20 +0200

    main file and index file

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void CreateIndexFileFile (char* filename, int numberOfRecords, int m);
+int InsertNewRecordAtIndex (char* filename, int RecordID, int Reference);
+void DeleteRecordFromIndex (char* filename, int RecordID);
+void DisplayIndexFileContent (char* filename);
+int SearchARecord (char* filename, int RecordID);
+
+int main(){
+
+}
+
+void CreateIndexFileFile (char* filename, int numberOfRecords, int m){
+
+}
+int InsertNewRecordAtIndex (char* filename, int RecordID, int Reference){
+
+}
+void DeleteRecordFromIndex (char* filename, int RecordID){
+
+}
+void DisplayIndexFileContent (char* filename){
+
+}
+int SearchARecord (char* filename, int RecordID){
+
+}
