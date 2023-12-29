6b9bdb5bcae05a9359b1076f14697414d5bae6ed
Shefaa Sumait <83834378+Shefaabi@users.noreply.github.com>
2023-12-29 12:49:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -64,22 +64,23 @@ int main() {
     }
 }
 
-void CreateIndexFile (char* filename, int numberOfRecords, int m){
-    ofstream Btree;
-    Btree.open(filename, ios::in | ios::out | ios::binary | ios::app);
+// void CreateIndexFile (char* filename, int numberOfRecords, int m){
+//     ofstream Btree;
+//     Btree.open(filename, ios::in | ios::out | ios::binary | ios::app);
 
-    // from 0 to numberOfRecords - 2 as the last one is a special case (-1)
-    for (int i = 0; i < numberOfRecords - 1; i++) {
-        node Node(m, i + 1);
-        // write the node to the file
-        Btree.write((char *) &Node, sizeof(Node));
-    }
-    // the last node is a special case(-1)
-    node Node(m, -1);
-    Btree.write((char *) &Node, sizeof(Node));
+//     // from 0 to numberOfRecords - 2 as the last one is a special case (-1)
+//     for (int i = 0; i < numberOfRecords - 1; i++) {
+//         node Node(m, i + 1);
+//         // write the node to the file
+//         Btree.write((char *) &Node, sizeof(Node));
+//     }
+//     // the last node is a special case(-1)
+//     node Node(m, -1);
+//     Btree.write((char *) &Node, sizeof(Node));
 
-    Btree.close();
-}
+//     Btree.close();
+// }
+
 
 int InsertNewRecordAtIndex (char* filename, int RecordID, int Reference){
 
@@ -89,23 +90,100 @@ void DeleteRecordFromIndex (char* filename, int RecordID){
 
 }
 
-void DisplayIndexFileContent (char* filename){
-    fstream Btree;
-    Btree.open(filename, ios::in | ios::out | ios::binary | ios::app);
+void CreateIndexFileFile(const char *filename, int numNodes, int m)
+{
+    ofstream outFile(filename, ios::binary);
 
-    Btree.seekg(0, ios::beg);
-    node Node;
-    // read the node from the file
-    while (Btree.read((char *) &Node, sizeof(Node))) {
-        cout << Node.status << " ";
-        for (int i = 0; i < Node.size; ++i) {
-            cout << Node.arr[i] << " ";
+    if (!outFile.is_open())
+    {
+        cerr << "Error opening file for writing." << endl;
+        return;
+    }
+
+    int cellNonNode = -1;
+    outFile.write((char*)(&cellNonNode), sizeof(cellNonNode));
+
+    int nextFreeNodeIndex = 1;
+    outFile.write((char*)(&nextFreeNodeIndex), sizeof(nextFreeNodeIndex));
+
+
+    for (int i = 0; i < (2 * m - 1); ++i)
+    {
+        outFile.write((char*)(&cellNonNode), sizeof(cellNonNode));
+    }
+
+    Node defaultNode(m);
+    for (int i = 1; i < numNodes; ++i)
+    {
+        // all uninitialized yet type = -1
+        outFile.write((char*)(&defaultNode.type), sizeof(defaultNode.type));
+        for (const auto &kv : defaultNode.keyValuePairs)
+        {
+            int k = kv.first;
+            outFile.write((char*)(&k), sizeof(k));
+            int v = kv.second;
+            outFile.write((char*)(&v), sizeof(v));
+        }
+    }
+
+    outFile.close();
+}
+
+void DisplayIndexFileContent(const char *filename)
+{
+    ifstream inFile(filename, ios::binary);
+    if (!inFile.is_open())
+    {
+        cerr << "Error opening file for reading." << endl;
+        return;
+    }
+
+    cout << "I | T | ";
+    for (int i = 1; i <= m; ++i)
+    {
+        cout << "K" << i << " | ";
+        cout << "R" << i << " | ";
+    }
+    cout << endl;
+
+    int index = 0;
+    Node node(m);
+    while (inFile.read(reinterpret_cast<char *>(&node.type), sizeof(node.type)))
+    {
+        cout << index++ << " | " << (node.type == -1 ? "X" : (node.type == 0 ? "Y" : "N")) << " | ";
+        for (int i = 0; i < m; ++i)
+        {
+            int key, value;
+            inFile.read(reinterpret_cast<char *>(&key), sizeof(key));
+            inFile.read(reinterpret_cast<char *>(&value), sizeof(value));
+            cout << key << " | " << value << " | ";
         }
         cout << endl;
     }
 
-    Btree.close();
+    cout << "\n\nI=Index | T=Node Type (X=unintialized [= -1], Y=YES [leaf], N=NO [non leaf])\n K=Key | R=reference/value" << endl;
+
+
+    inFile.close();
 }
+
+// void DisplayIndexFileContent (char* filename){
+//     fstream Btree;
+//     Btree.open(filename, ios::in | ios::out | ios::binary | ios::app);
+
+//     Btree.seekg(0, ios::beg);
+//     node Node;
+//     // read the node from the file
+//     while (Btree.read((char *) &Node, sizeof(Node))) {
+//         cout << Node.status << " ";
+//         for (int i = 0; i < Node.size; ++i) {
+//             cout << Node.arr[i] << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     Btree.close();
+// }
 
 int SearchARecord (char* filename, int RecordID){
 
